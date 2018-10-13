583c4eb31b4a003a43409f73d8379649cb012f34
epam <epam@EPRUIZHL0029.moscow.epam.com>
2018-10-13 14:22:10 +0400

    Target framewroks

--- src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
@@ -1,16 +1,39 @@
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace EPAM.Core.ReportHelper.Tests
 {
     [TestClass]
     public class ReportNameHelperTests
     {
+
+        static object[][] NormalizeFileNameTestData
+        {
+            get
+            {
+                object[][] data = new object[][]
+                {
+            new object[]{"Hello: my world", '_', "Hello_ my world"},
+            new object[]{"Hello my %world", '_', "Hello my %world"},
+            new object[]{"Hello my /world", '_', "Hello my _world"}
+                };
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    data[0][2] = "Hello: my world";
+                    data[1][2] = "Hello my %world";
+                    data[2][2] = "Hello my _world";
+                }
+                return data;
+            }
+        }
         [TestMethod]
         //[DataRow("Hello: my world", '_', "Hello_ my world")]
-        [DataRow("Hello my %world", '_', "Hello my %world")]
-        [DataRow("Hello my /world", '_', "Hello my _world")]
+        //[DataRow("Hello my %world", '_', "Hello my %world")]
+        //[DataRow("Hello my /world", '_', "Hello my _world")]
+        [DynamicData("NormalizeFileNameTestData", DynamicDataSourceType.Property)]  
         public void NormalizeFileNameTest(string name, char repl, string expected)
         {
             var result = ReportNameHelper.NormalizeFileName(name, repl);
