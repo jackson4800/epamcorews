a5695684602972f0d52b5236b21579394e4d63ee
epam <epam@EPRUIZHL0029.moscow.epam.com>
2018-10-13 14:22:23 +0400

    Target framewroks

---
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ArrayHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPAM.CoreWorkshop.ReportHelper.Tests
+{
+    public static class ArrayHelper
+    {
+        public static void Fill<T>(T[] array, T value)
+        {
+#if NETCOREAPP2_0 || NETCOREAPP2_1
+            Array.Fill<T>(array, value);
+#else
+            for (var i = array.GetLowerBound(0); i <= array.GetUpperBound(0); i++)
+            {
+                array[i] = value;
+            }
+#endif
+        }
+    }
+}
