2edfef37b883732354de770f0c97f8a65b7070b7
epam <epam@EPRUIZHL0029.moscow.epam.com>
2018-10-13 16:28:11 +0400

    Offline ver of site

---
+++ src/EPAM.CoreWorkshop.NorthwindLib/Migrations/20181013110701_AddCategoryOwnerisGay.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EPAM.CoreWorkshop.NorthwindLib.Migrations
+{
+    public partial class AddCategoryOwnerisGay : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Gay",
+                table: "Categories",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gay",
+                table: "Categories");
+        }
+    }
+}
